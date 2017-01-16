7c2c61c9e17d130fccd5630edd384cd7d89fcbb6
Bobby Miller <miller.bobby@gmail.com>
2017-01-16 08:41:20 -0500

    Initial Commit

---
+++ oee/settings/production.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, unicode_literals
+
+from .base import *
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+try:
+    from .local import *
+except ImportError:
+    pass
